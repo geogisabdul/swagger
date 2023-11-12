94fec563c8f8ea081a4799b7923426300c3297f2
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-11-12 18:23:06 +0800

    main

--- js/swaggerfunc.js
+++ js/swaggerfunc.js
@@ -1,20 +1,15 @@
-import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
+import SwaggerUIBundle from "https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js";
 
-export let URLData = "https://raw.githubusercontent.com/GIS-RIZIQ/swagger/main/yaml/openapi.yaml";
+export let URLData = "https://raw.githubusercontent.com/geogisabdul/swagger/main/yaml/openapi.yaml";
 
 export const UIData = SwaggerUIBundle({
-    url: URLData, //Location of Open API spec in the repo
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-        SwaggerUIBundle.presets.apis,
-        SwaggerUIBundle.SwaggerUIStandalonePreset
-    ],
-    plugins: [
-        SwaggerUIBundle.plugins.DownloadUrl
-    ],
+  url: URLData, //Location of Open API spec in the repo
+  dom_id: "#swagger-ui",
+  deepLinking: true,
+  presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset],
+  plugins: [SwaggerUIBundle.plugins.DownloadUrl],
 });
 
 export function setSwagger() {
-    return UIData
+  return UIData;
 }
